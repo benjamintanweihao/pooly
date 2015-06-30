f512aaef7671208d7800af1b0c50528d47a134d8
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-07-01 07:47:12 +0800

    WIP!

---
+++ lib/pooly/pool_supervisor.ex
@@ -0,0 +1,20 @@
+defmodule Pooly.PoolSupervisor do
+  use Supervisor
+
+  def start_link(pool_config) do
+    Supervisor.start_link(__MODULE__, [pool_config])
+  end
+
+  def init(pool_config) do
+    opts = [
+      strategy: :one_for_all
+    ]
+
+    children = [
+      worker(Pooly.PoolServer, [self, pool_config])
+    ]
+
+    supervise(children, opts)
+  end
+
+end
--- lib/pooly/supervisor.ex
+++ lib/pooly/supervisor.ex
@@ -1,14 +1,14 @@
 defmodule Pooly.Supervisor do
   use Supervisor
 
-  def start_link(pool_config) do
-    Supervisor.start_link(__MODULE__, pool_config)
+  def start_link(pools_config) do
+    Supervisor.start_link(__MODULE__, pools_config)
   end
 
-  def init(pool_config) do
+  def init(pools_config) do
     children = [
-      supervisor(Pooly.WorkersSupervisor, []),
-      worker(Pooly.Server, [pool_config])
+      supervisor(Pooly.PoolsSupervisor, []),
+      worker(Pooly.Server, [pools_config])
     ]
 
     opts = [strategy: :one_for_all,
