dcab58fd4fa6310fb86c12503ac5dedbda024021
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-07-01 10:30:16 +0800

    Pooly.PoolServer should be passed a `pool_config`, not `[pool_config]`.

--- lib/pooly/pool_supervisor.ex
+++ lib/pooly/pool_supervisor.ex
@@ -2,7 +2,7 @@ defmodule Pooly.PoolSupervisor do
   use Supervisor
 
   def start_link(pool_config) do
-    Supervisor.start_link(__MODULE__, [pool_config])
+    Supervisor.start_link(__MODULE__, pool_config)
   end
 
   def init(pool_config) do
