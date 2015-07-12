d3e92fe792d549914626815a8b08c0d6aa0510cb
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-07-12 17:25:37 +0800

    Fix units.

--- lib/pooly/worker_supervisor.ex
+++ lib/pooly/worker_supervisor.ex
@@ -13,7 +13,7 @@ defmodule Pooly.WorkerSupervisor do
     children = [worker(m, a, worker_opts)]
     opts     = [strategy:     :simple_one_for_one,
                 max_restarts: 5,
-                max_seconds:  3600]
+                max_seconds:  5]
 
     supervise(children, opts)
   end
