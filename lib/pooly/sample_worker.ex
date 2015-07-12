efd847c7115d6ab0b00de9add0887370dc857b4d
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-07-12 17:06:16 +0800

    Update sample worker.

--- lib/pooly/sample_worker.ex
+++ lib/pooly/sample_worker.ex
@@ -5,8 +5,17 @@ defmodule SampleWorker do
     GenServer.start_link(__MODULE__, :ok, [])
   end
 
+  def stop(pid) do
+    GenServer.call(pid, :stop)
+  end
+
+
   def work_for(pid, duration) do
     GenServer.cast(pid, {:work_for, duration})
+  end
+
+  def handle_call(:stop, _from, state) do
+    {:stop, :normal, :ok, state}
   end
 
   def handle_cast({:work_for, duration}, state) do
@@ -15,3 +24,4 @@ defmodule SampleWorker do
   end
 
 end
+
