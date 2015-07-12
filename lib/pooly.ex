e46af1e7558fbdc7cd092772b1e14691048d5189
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2015-07-12 17:10:22 +0800

    Tidy things up a little.

--- lib/pooly.ex
+++ lib/pooly.ex
@@ -2,10 +2,7 @@ defmodule Pooly do
   use Application
 
   def start(_type, _args) do
-    # This is an example on how to start the Pooly
-    pool_config = [mfa: {SampleWorker, :start_link, []},
-                   size: 5]
-
+    pool_config = [mfa: {SampleWorker, :start_link, []}, size: 5]
     start_pool(pool_config)
   end
 
